3768a00eb2df8db3601f053b22a529d2e73daf21
GuillaumeVerpoest <guillaume.verpoest@gmail.com>
2020-05-25 12:53:05 +0200

    alfredv1 End

--- bot_main_alfredv1.py
+++ bot_main_alfredv1.py
@@ -1,17 +1,19 @@
 # import discord
 import discord
 from discord.ext import commands
+from bot_planning_alfredv1 import get_hour_in_planning, Planning_Alfredv1
 
 
 # jeton de alfredv1
-jeton = "NzEzMDA0NjQyNDM0NTQ3NzE1.XsZ03w.pAtjESUygBx1oNVYgbtQq7AnlxE"
+jeton = "NzEzMDA0NjQyNDM0NTQ3NzE1.XsuMfQ.XyIN1DtA-GPoUJ550uFqQh3evhI"
 bot = commands.Bot(command_prefix=('!'))
+verif_categorie = ["Code","Anglais","Devolepement_Perso", "Libre"]
 
 # eveil de alfredv1
 @bot.event
 async def on_ready():
     print("bot pret")
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Game("Creation en cour"))
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Game("En court de cration"))
     #await sa_montre()
 
 
@@ -22,12 +24,18 @@ async def chrono(ctx):
 
 @bot.command()
 async def planning(ctx, categorie):
-    if(categorie == "stop"):
-        await ctx.send("lance la function stop")
-    elif categorie == "Code":
-        await ctx.send("lance le timer dans le bonne colonne ")
-    else:
-        await ctx.send("error !planning")
+    day_planning = Planning_Alfredv1().get_planning_day()
+    time = Planning_Alfredv1().add_planning(day_planning, categorie)
+    if (categorie in verif_categorie):
+        await ctx.send(time)
+
+@bot.command()
+async def stop(ctx, categorie):
+    day_planning = Planning_Alfredv1().get_planning_day()
+    time = Planning_Alfredv1().add_planning(day_planning, categorie)
+    if (categorie in verif_categorie):
+        Planning_Alfredv1().stop(day_planning, categorie, time)
+        await ctx.send("J'arrete le chrono")
 
 @planning.error
 async def on_command_error(ctx, error):
--- bot_planning_alfredv1.py
+++ bot_planning_alfredv1.py
@@ -1,15 +1,22 @@
-from time import strftime
+from time import strftime, localtime
 from datetime import timedelta
 from datetime import datetime
 import time
 import pandas as pd
+
+def get_hour_in_planning(index):
+    array = index.split(':')
+    delta = timedelta(hours = int(array[0]), minutes = int(array[1]))
+    return(delta)
+
+
 
 class Planning_Alfredv1:
 
     def __init__(self):
         self.planning = pd.read_csv("planning.csv", index_col = [0])
 
-    def planning_day(self):
+    def get_planning_day(self):
 
         date_of_day = strftime("%d:%m:%Y")
         get_day = self.planning.loc[self.planning["Date"] == date_of_day].shape[0]
@@ -22,34 +29,25 @@ class Planning_Alfredv1:
         else:
             return(line_today-1)
 
-    def add_planning(self, categorie, day):
-        hour_now = timedelta (hours = time.localtime().tm_hour, minutes = time.localtime().tm_min)
-        cheked_emply = pd.isnull(self.planning.loc[day, categorie])
+    def add_planning(self, day_planning, categorie):
+        hour_of_start = timedelta (hours = time.localtime().tm_hour, minutes = time.localtime().tm_min)
 
-        if(cheked_emply == True):
-            self.planning.loc[day, categorie] = hour_now
+        if(pd.isnull(self.planning.loc[day_planning, categorie]) == True):
+            self.planning.loc[day_planning, categorie] = hour_of_start
             self.planning.to_csv("planning.csv")
+            return("initialisation du chrono")
         else:
-            get_hour_in_planning = self.planning.loc[day, categorie].split(':')
-            hour = timedelta (hours = int(get_hour_in_planning[0]), minutes = int(get_hour_in_planning[1]))
-            return(hour_now - hour)
+            hour = get_hour_in_planning(self.planning.loc[day_planning, categorie])
+            return(hour_of_start - hour)
 
-    def stop(self, day, categorie, time):
-        cheked_emply = pd.isnull(self.planning.loc[day, categorie])
-        hour2 = timedelta(hours= 0, minutes=0)
-        if(cheked_emply == False):
-            get_hour_in_planning2 = self.planning.loc[day,"temp " + categorie].split(':')
-            hour2 = timedelta (hours = int(get_hour_in_planning2[0]), minutes = int(get_hour_in_planning2[1]))
-        print(hour2)
-        self.planning.loc[day,"temp "+ categorie] =  hour2 + time
-        self.planning.loc[day, categorie] = None
+    def stop(self, day_planning, categorie, time):
+        get_hour = timedelta(hours= 0, minutes=0)
+        if(pd.isnull(self.planning.loc[day_planning, "temp "+ categorie]) == False):
+            get_hour = get_hour_in_planning(self.planning.loc[day_planning,"temp " + categorie])
+        self.planning.loc[day_planning,"temp "+ categorie] =  get_hour + time
+        self.planning.loc[day_planning, categorie] = None
         self.planning.to_csv("planning.csv")
 
-alfred = Planning_Alfredv1()
-day = alfred.planning_day()
-time = alfred.add_planning("Code", day)
-print(time)
-alfred.stop(day, "Code", time)
 
 
 
